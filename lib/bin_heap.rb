61d8850a8a378b37b52908c050773e8b71c6b64a
Philip Corliss <pcorliss@bigdevbox.com>
2021-04-29 22:05:32 +0000

    Simple Binary Heap Implementation

---
+++ lib/bin_heap.rb
@@ -0,0 +1,84 @@
+class BinHeap
+  def initialize(t = :min)
+    @t = t
+    @arr = []
+  end
+
+  def push(n)
+    val = _min? ? n : -n
+    @arr.push(val)
+    _bubble_up!(@arr.length - 1)
+  end
+
+  def peek
+    n = @arr.first
+    _min? ? n : -n
+  end
+
+  def pop
+    _swap(0, @arr.length - 1)
+    n = @arr.pop
+    _bubble_down(0)
+    _min? ? n : -n
+  end
+
+  def to_a
+    @arr
+  end
+
+  def size
+    @arr.size
+  end
+
+  def _min?
+    @t == :min
+  end
+
+  def _parent(idx)
+    level = Math.log2(idx + 1).to_i
+    offset = (idx - ((2 ** level) - 1)) / 2
+    offset + (2 ** (level - 1)) - 1
+  end
+
+  def _child(idx)
+    level = Math.log2(idx + 1).to_i
+    offset = (idx - ((2 ** level) - 1)) * 2
+    offset + (2 ** (level + 1)) - 1
+  end
+
+  def _swap(a_idx, b_idx)
+    tmp = @arr[a_idx]
+    @arr[a_idx] = @arr[b_idx]
+    @arr[b_idx] = tmp
+  end
+
+  def _bubble_down(idx)
+    a = @arr[idx]
+    b_idx = _child(idx)
+    c_idx = b_idx + 1
+    b = @arr[b_idx]
+    c = @arr[c_idx]
+    # puts "A: #{a} B: #{b} C: #{c} - #{to_a}"
+    if (b && a > b) || (c && a > c)
+      if !c || b < c
+        # puts "\tSwap: a and b"
+        _swap(idx, b_idx)
+        _bubble_down(b_idx)
+      else
+        # puts "\tSwap: a and c"
+        _swap(idx, c_idx)
+        _bubble_down(c_idx)
+      end
+    end
+  end
+
+  def _bubble_up!(idx)
+    a = @arr[idx]
+    p_idx = _parent(idx)
+    p = @arr[p_idx]
+    if a < p
+      _swap(idx, p_idx)
+      _bubble_up!(p_idx)
+    end
+  end
+end
---
+++ spec/bin_heap_spec.rb
@@ -0,0 +1,129 @@
+require './lib/bin_heap.rb'
+
+describe BinHeap do
+  subject { BinHeap.new }
+
+  describe '#new' do
+    it 'inits a heap' do
+      expect(subject).to be_a(BinHeap)
+    end
+
+    it 'inits a min heap' do
+      subject.push(3)
+      subject.push(2)
+      subject.push(1)
+      expect(subject.peek).to eq(1)
+    end
+
+    it 'inits a max heap' do
+      subject = BinHeap.new(:max)
+      subject.push(3)
+      subject.push(2)
+      subject.push(1)
+      expect(subject.peek).to eq(3)
+    end
+  end
+
+  describe '#peek' do
+    it 'returns the smallest element' do
+      subject.push(3)
+      subject.push(2)
+      subject.push(1)
+      expect(subject.peek).to eq(1)
+    end
+
+    it "doesn't remove the element" do
+      subject.push(2)
+      subject.peek
+      expect(subject.size).to eq(1)
+    end
+  end
+
+  describe '#pop' do
+    it 'returns the smallest element' do
+      subject.push(3)
+      subject.push(2)
+      subject.push(1)
+      expect(subject.pop).to eq(1)
+    end
+
+    it "removes the element" do
+      subject.push(2)
+      subject.pop
+      expect(subject.size).to eq(0)
+    end
+  end
+
+  describe '#push' do
+    it 'adds an element' do
+      subject.push(5)
+      expect(subject.to_a).to eq([5])
+    end
+  end
+
+  describe '#to_a' do
+    it 'returns the heap' do
+      subject.push(3)
+      subject.push(2)
+      subject.push(1)
+      expect(subject.to_a).to eq([1,3,2])
+    end
+  end
+
+  describe '#size' do
+    it 'returns the current size of the heap' do
+      subject.push(3)
+      subject.push(2)
+      expect(subject.size).to eq(2)
+    end
+  end
+
+  describe '#_parent' do
+    it 'returns the idx of the parent node in a binary tree' do
+      expect(subject._parent(1)).to eql(0)
+      expect(subject._parent(2)).to eql(0)
+      expect(subject._parent(3)).to eql(1)
+      expect(subject._parent(4)).to eql(1)
+      expect(subject._parent(5)).to eql(2)
+      expect(subject._parent(6)).to eql(2)
+    end
+  end
+
+  describe '#_child' do
+    it 'returns the idx of the first child node in a binary tree' do
+      expect(subject._child(0)).to eql(1)
+      expect(subject._child(1)).to eql(3)
+      expect(subject._child(2)).to eql(5)
+      expect(subject._child(3)).to eql(7)
+      expect(subject._child(4)).to eql(9)
+      expect(subject._child(5)).to eql(11)
+      expect(subject._child(6)).to eql(13)
+    end
+  end
+
+  context 'validation' do
+    it 'returns a sorted large list' do
+      n = 1000
+      range = 100
+      arr = n.times.map do 
+        val = rand(range)
+        subject.push(val)
+        val
+      end
+
+      arr.sort!
+
+      # puts "Heap Start: #{subject.to_a}"
+
+      heap_arr = n.times.map do |i|
+        val = subject.pop
+        # puts "#{i} - #{val} #{subject.to_a}"
+        val
+      end
+
+      # puts arr.inspect
+      # puts heap_arr.inspect
+      expect(heap_arr).to eq(arr)
+    end
+  end
+end
